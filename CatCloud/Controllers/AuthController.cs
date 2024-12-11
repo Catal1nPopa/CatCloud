5c73fbf58b347f4b1717cb732609dcd3034216fe
Catal1nPopa <misterco2002@gmail.com>
2024-12-11 08:14:35 +0200

    Project initiation

---
+++ CatCloud/Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatCloud.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+        [HttpGet("Login")]
+        public ActionResult<string> Get()
+        {
+            return Ok("ss");
+        }
+    }
+}
