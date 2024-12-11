bc24dc084d784f57f616255ab99538c1b8d260f6
popa.catalin <popa.catalin@moldtelecom.md>
2024-12-11 16:09:59 +0200

    Serilog

---
+++ Helper/Serilog/LoggerHelper.cs
@@ -0,0 +1,27 @@
+﻿using Serilog;
+
+namespace Helper.Serilog
+{
+    public static class LoggerHelper
+    {
+        public static void LogInformation(string message)
+        {
+            Log.Information(message);
+        }
+
+        public static void LogWarning(string message)
+        {
+            Log.Warning(message);
+        }
+
+        public static void LogError(Exception ex, string message)
+        {
+            Log.Error(ex, message);
+        }
+
+        public static void LogDebug(string message)
+        {
+            Log.Debug(message);
+        }
+    }
+}
